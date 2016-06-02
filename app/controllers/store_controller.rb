fa43a427f431774cfbe8190a3b26d0ead85a38e7
João Ferreira <jacferreira@gmail.com>
2016-06-02 19:58:05 +0100

    Task D + playtime finished

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now
+    
+    # playtime task D
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+    
+    @counter = session[:counter] if session[:counter] > 5
   end
 end
