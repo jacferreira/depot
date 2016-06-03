19ffe5eb2384022880f21ab209c07d34b69ab186
João Ferreira <jacferreira@gmail.com>
2016-06-03 15:14:05 +0100

    tasks F and G finished

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,9 +54,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
@@ -64,7 +66,12 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+      begin
+        @cart = Cart.find(params[:id])
+      rescue
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_url, notice: 'Invalid cart'
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
