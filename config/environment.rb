220f7e1d4964a1fbe55adaf7d76e0bf1f530ebff
João Ferreira <jacferreira@gmail.com>
2016-06-05 23:25:16 +0100

    task H finished

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           586,
+    domain:         "example.com",
+    authentication: "plain",
+    user_name:      "jacferreira@gmail.com",
+    password:       "asd",
+    enable_starttls_auto: true
+  }
+end
